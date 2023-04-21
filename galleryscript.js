d4fbc415c206a4d8e0ed751fab1ec6bc31c46e4f
Minghao <u7227667@anu.edu.au>
2023-04-21 11:46:43 +1000

    new festures photo gallery image maps

--- galleryscript.js
+++ galleryscript.js
@@ -1,5 +1,29 @@
+// ----------------
 let slideIndex = 1;
-showSlides(slideIndex);
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("photogallery");
+  let dots = document.getElementsByClassName("slides");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  //console.log("slideIndex after bounds check:", slideIndex);
+  //console.log("slides check",slides)
+  //console.log("slideIndex after bounds check:", slides[slideIndex-1]);
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+// showSlides(slideIndex);
 
 
 // Next/previous controls
@@ -12,20 +36,8 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("photogallery");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+
+// Call initGalleries function initially to preload the first slide and start the automatic slideshow
+window.onload = function() {
+  showSlides(1);
 }
